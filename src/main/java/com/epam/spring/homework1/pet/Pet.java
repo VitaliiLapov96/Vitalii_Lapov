bb33c542531cf0e3c09f9964cb3c32b7dd731ab7
Vitalii Lapov <vitalii.lapov.96@gmail.com>
2021-11-09 18:09:22 +0200

    Add injection list of animals for Pet

--- src/main/java/com/epam/spring/homework1/pet/Pet.java
+++ src/main/java/com/epam/spring/homework1/pet/Pet.java
@@ -1,8 +1,19 @@
 package com.epam.spring.homework1.pet;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class Pet {
+
+    @Autowired
+    private List<Animal> pets;
+
+    public void printPets() {
+        for (Animal pet : pets)
+            System.out.println(pet.getAnimal());
+    }
 
 }
