a68c10aadf7c83b735a5888389dde316120c4568
Vitalii Lapov <vitalii.lapov.96@gmail.com>
2021-11-09 18:14:55 +0200

    Add component scan without Spider bean for PetConfig

--- src/main/java/com/epam/spring/homework1/config/PetConfig.java
+++ src/main/java/com/epam/spring/homework1/config/PetConfig.java
@@ -1,8 +1,12 @@
 package com.epam.spring.homework1.config;
 
+import com.epam.spring.homework1.pet.Spider;
 import org.springframework.context.annotation.*;
 
 @Configuration
+@ComponentScan(basePackages = {"com.epam.spring.homework1.pet"},
+        excludeFilters = @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE, value = Spider.class))
 public class PetConfig {
 
 }
