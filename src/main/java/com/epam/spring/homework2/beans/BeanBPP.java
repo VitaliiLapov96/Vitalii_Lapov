ad50a9de167182948bbb56cfee7b5f7ae9904755
Vitalii Lapov <vitalii.lapov.96@gmail.com>
2021-11-11 18:19:07 +0200

    BeanBPP implements BeanPostProcessor

--- src/main/java/com/epam/spring/homework2/beans/BeanBPP.java
+++ src/main/java/com/epam/spring/homework2/beans/BeanBPP.java
@@ -1,8 +1,9 @@
 package com.epam.spring.homework2.beans;
 
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BeanBPP {
+public class BeanBPP implements BeanPostProcessor {
 
 }
