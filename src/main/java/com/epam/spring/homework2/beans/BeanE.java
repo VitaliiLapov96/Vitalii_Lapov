3966dbf839170c274c772b202a99da1b22069de2
Vitalii Lapov <vitalii.lapov.96@gmail.com>
2021-11-11 17:48:30 +0200

    BeanE implements methods init and destroy via annotation

--- src/main/java/com/epam/spring/homework2/beans/BeanE.java
+++ src/main/java/com/epam/spring/homework2/beans/BeanE.java
@@ -1,5 +1,11 @@
 package com.epam.spring.homework2.beans;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static com.epam.spring.homework2.constants.ApplicationConstants.DESTROYED;
+import static com.epam.spring.homework2.constants.ApplicationConstants.INITIALIZED;
+
 public class BeanE {
 
     private String name;
@@ -19,6 +25,16 @@ public class BeanE {
 
     public void setValue(int value) {
         this.value = value;
+    }
+
+    @PostConstruct
+    private void init() {
+        System.out.println(this.getClass().getSimpleName() + " " + INITIALIZED + " via @PostConstruct");
+    }
+
+    @PreDestroy
+    private void destroy() {
+        System.out.println(this.getClass().getSimpleName() + " " + DESTROYED + " via @PreDestroy");
     }
 
     @Override
