e43d432f43cac593d55c042b85a71758798e15ea
Vitalii Lapov <vitalii.lapov.96@gmail.com>
2021-11-11 17:34:58 +0200

    Add ApplicationConstants

---
+++ src/main/java/com/epam/spring/homework2/constants/ApplicationConstants.java
@@ -0,0 +1,8 @@
+package com.epam.spring.homework2.constants;
+
+public class ApplicationConstants {
+
+    public static final String INITIALIZED = "initialized";
+    public static final String DESTROYED = "destroyed";
+
+}
