1ad8673d38f95d1603900ddf0b7c56624be96a01
Vitalii Lapov <vitalii.lapov.96@gmail.com>
2021-11-11 18:28:56 +0200

    Ensure order initialization for BeanD, BeanB, BeanC

--- src/main/java/com/epam/spring/homework2/beans/BeanBFPP.java
+++ src/main/java/com/epam/spring/homework2/beans/BeanBFPP.java
@@ -4,9 +4,11 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
 @Component
+@DependsOn({"beanD", "beanB", "beanC"})
 public class BeanBFPP implements BeanFactoryPostProcessor {
 
     @Override
