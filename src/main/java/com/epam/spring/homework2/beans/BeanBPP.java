96ac060229ee12ea4c1e6cd313a44ca107f9ddbd
Vitalii Lapov <vitalii.lapov.96@gmail.com>
2021-11-11 20:49:38 +0200

    BeanBPP implements methods, especially validating all beans

--- src/main/java/com/epam/spring/homework2/beans/BeanBPP.java
+++ src/main/java/com/epam/spring/homework2/beans/BeanBPP.java
@@ -1,9 +1,24 @@
 package com.epam.spring.homework2.beans;
 
+import com.epam.spring.homework2.validators.MyValidator;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BeanBPP implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof MyValidator) {
+            ((MyValidator) bean).validate();
+        }
+        return bean;
+    }
 
 }
