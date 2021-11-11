b38f8475a6d467e1835fba216d880823d1ee5ca1
Vitalii Lapov <vitalii.lapov.96@gmail.com>
2021-11-11 16:58:26 +0200

    Add fields name, value for beans BeanA, BeanB, BeanC, BeanD, BeanE, BeanF

--- src/main/java/com/epam/spring/homework2/beans/BeanA.java
+++ src/main/java/com/epam/spring/homework2/beans/BeanA.java
@@ -1,4 +1,32 @@
 package com.epam.spring.homework2.beans;
 
 public class BeanA {
+
+    private String name;
+    private int value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "BeanA{" +
+                "name='" + name + '\'' +
+                ", value=" + value +
+                '}';
+    }
+
 }
--- src/main/java/com/epam/spring/homework2/beans/BeanE.java
+++ src/main/java/com/epam/spring/homework2/beans/BeanE.java
@@ -1,4 +1,32 @@
 package com.epam.spring.homework2.beans;
 
 public class BeanE {
+
+    private String name;
+    private int value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "BeanE{" +
+                "name='" + name + '\'' +
+                ", value=" + value +
+                '}';
+    }
+
 }
