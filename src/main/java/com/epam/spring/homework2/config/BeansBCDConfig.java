932ce5b74147081c5130b2e47af1c888e868fc02
Vitalii Lapov <vitalii.lapov.96@gmail.com>
2021-11-11 17:29:19 +0200

    Register BeanA, BeanB, BeanC, BeanD, BeanE, BeanF in BeansBCDConfig

--- src/main/java/com/epam/spring/homework2/config/BeansBCDConfig.java
+++ src/main/java/com/epam/spring/homework2/config/BeansBCDConfig.java
@@ -1,9 +1,43 @@
 package com.epam.spring.homework2.config;
 
+import com.epam.spring.homework2.beans.*;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 @ComponentScan("com.epam.spring.homework2.beans")
 public class BeansBCDConfig {
+
+    @Bean
+    public BeanD beanD() {
+        return new BeanD();
+    }
+
+    @Bean
+    public BeanB beanB() {
+        return new BeanB();
+    }
+
+    @Bean
+    public BeanC beanC() {
+        return new BeanC();
+    }
+
+    @Bean
+    public BeanA beanA() {
+        return new BeanA();
+    }
+
+    @Bean
+    public BeanE beanE() {
+        return new BeanE();
+    }
+
+    @Bean
+    public BeanF beanF() {
+        return new BeanF();
+    }
+
 }
