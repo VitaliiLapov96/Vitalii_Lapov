de012cd2f2e2eb15a4d3f01663f0269a8f5c826d
Vitalii Lapov <vitalii.lapov.96@gmail.com>
2021-11-11 18:23:28 +0200

    Add new constants for ApplicationConstants

--- src/main/java/com/epam/spring/homework2/constants/ApplicationConstants.java
+++ src/main/java/com/epam/spring/homework2/constants/ApplicationConstants.java
@@ -4,5 +4,7 @@ public class ApplicationConstants {
 
     public static final String INITIALIZED = "initialized";
     public static final String DESTROYED = "destroyed";
+    public static final String MSG_NAME = ">>> field name must be not null";
+    public static final String MSG_VALUE = ">>> field value must not contain a negative number";
 
 }
