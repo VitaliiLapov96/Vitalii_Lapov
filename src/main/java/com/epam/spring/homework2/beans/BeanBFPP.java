6d9c16769ff497be3b61d1506603168a6e64e4f8
Vitalii Lapov <vitalii.lapov.96@gmail.com>
2021-11-11 18:14:32 +0200

    BeanBFPP implements BeanFactoryPostProcessor

--- src/main/java/com/epam/spring/homework2/beans/BeanBFPP.java
+++ src/main/java/com/epam/spring/homework2/beans/BeanBFPP.java
@@ -1,8 +1,16 @@
 package com.epam.spring.homework2.beans;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BeanBFPP {
+public class BeanBFPP implements BeanFactoryPostProcessor {
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+
+    }
 
 }
