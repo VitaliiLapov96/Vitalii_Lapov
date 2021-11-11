9d356a74b2ada220977d92dcef55f21f19f9bb14
Vitalii Lapov <vitalii.lapov.96@gmail.com>
2021-11-11 18:15:49 +0200

    BeanBFPP change method from init to otherInit for BeanB

--- src/main/java/com/epam/spring/homework2/beans/BeanBFPP.java
+++ src/main/java/com/epam/spring/homework2/beans/BeanBFPP.java
@@ -1,6 +1,7 @@
 package com.epam.spring.homework2.beans;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,8 @@ public class BeanBFPP implements BeanFactoryPostProcessor {
 
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
-
+        BeanDefinition beanDefinition = configurableListableBeanFactory.getBeanDefinition("beanB");
+        beanDefinition.setInitMethodName("otherInit");
     }
 
 }
