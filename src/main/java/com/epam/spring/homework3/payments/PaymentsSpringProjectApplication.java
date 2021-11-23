73f144fff134891face428bdbd4a6b709e916600
Vitalii Lapov <vitalii.lapov.96@gmail.com>
2021-11-23 11:22:11 +0200

    Create Payments Spring project

---
+++ src/main/java/com/epam/spring/homework3/payments/PaymentsSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.epam.spring.homework3.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsSpringProjectApplication.class, args);
+	}
+
+}
